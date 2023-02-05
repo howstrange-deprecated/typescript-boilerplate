bb7047b2e192dcdad922c7e70ac71326147f7e7d
Alan Silva <123318858+howstrangeoficial@users.noreply.github.com>
2023-02-05 12:45:31 -0300

    Adicionando as configurações do eslint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+// Configurações do eslint para fazer lint no codigo typescript
+module.exports = {
+  // Configuração de root
+  root: true,
+  // Ambiente de execução
+  env: {
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  // eslint parser para typescript
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+  // Extensões de arquivos que serão analisados
+  overrides: [
+    {
+      files: ['*.ts', '*.js'],
+    }
+  ],
+  // plugins do eslint para typescript
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  // extends do eslint para typescript
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    //'plugin:prettier/recommended',
+  ],
+  // Eslint ignora
+  ignorePatterns: ['.eslintrc.js'],
+  // Regras do eslint para typescript
+  rules: {
+    // Regras basicas
+    indent: [
+      'error',
+      2
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    // regras avançadas
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    // Regras do typescript
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
